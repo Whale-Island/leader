7006e912351454805186779cb153b7241cd52552
siestacat <1109968833@qq.com>
2017-03-02 18:53:39 +0800

    spring、jpa

--- src/main/java/com/game/leader/HelloWorldClient.java
+++ src/main/java/com/game/leader/HelloWorldClient.java
@@ -9,12 +9,12 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -27,9 +27,16 @@ public class HelloWorldClient {
 
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
-					ch.pipeline().addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()))
-							.addLast("decoder", new StringDecoder()).addLast("encoder", new StringEncoder())
-							.addLast(new HelloClientHandler());
+					ChannelPipeline pipeline = ch.pipeline();
+					// 用protobuf的解码器
+					pipeline.addLast("framer", new ProtobufVarint32FrameDecoder());
+
+					// 字符串解码 和 编码
+					pipeline.addLast("decoder", new StringDecoder());
+					pipeline.addLast("encoder", new StringEncoder());
+
+					// 自己的逻辑Handler
+					pipeline.addLast("handler", new HelloClientHandler());
 				}
 
 			});
--- src/main/java/com/game/leader/server/GameServer.java
+++ src/main/java/com/game/leader/server/GameServer.java
@@ -1,80 +1,86 @@
 package com.game.leader.server;
 
-import java.net.InetAddress;
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.List;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import com.game.leader.server.bean.ShutdownListener;
+import com.game.leader.server.bean.ThreadAdapter;
+import com.game.leader.server.pool.ThreadPool;
 
 public class GameServer {
-	public void run() { // 配置服务端NIO线程组
-		EventLoopGroup bossGroup = new NioEventLoopGroup();
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		try {
-			ServerBootstrap b = new ServerBootstrap();
-			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
-					.handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
+	/** 日志 */
+	private Logger log = LoggerFactory.getLogger(GameServer.class);
+	/** 关服监听列表 */
+	private List<ShutdownListener> listeners;
+	private ThreadAdapter[] threadAdapters;
 
-						@Override
-						protected void initChannel(SocketChannel ch) throws Exception {
-							ChannelPipeline pipeline = ch.pipeline();
-							// 用protobuf的解码器
-							pipeline.addLast("framer", new ProtobufVarint32FrameDecoder());
+	public void run(AbstractApplicationContext applicationContext) throws IOException, CertificateException {
+		TcpServer tcpServer = new TcpServer(0);
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					ThreadPoolTaskScheduler scheduler = applicationContext.getBean("TaskScheduler",
+							ThreadPoolTaskScheduler.class);
+					scheduler.shutdown();
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+				}
+				try {
+					tcpServer.stop();
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+				}
+				try {
+					stop();
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+				}
+				ThreadPool.shutdown();
+				applicationContext.close();
+				log.info("Game Server Is Closed.");
+			}
+		}));
+		tcpServer.start();
+	}
 
-							// 字符串解码 和 编码
-							pipeline.addLast("decoder", new ProtobufDecoder(null, null));
-							pipeline.addLast("encoder", new StringEncoder());
-
-							// 自己的逻辑Handler
-							pipeline.addLast("handler", new ServerHandler());
-						}
-
-					});
-			// 绑定端口，同步等待成功
-			ChannelFuture f = b.bind(8000).sync();
-			// 等待服务端监听端口关闭
-			f.channel().closeFuture().sync();
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			// 退出时释放资源
-			bossGroup.shutdownGracefully();
-			workerGroup.shutdownGracefully();
+	/** 停服调用方法 */
+	private void stop() {
+		for (ShutdownListener serverListener : listeners) {
+			try {
+				serverListener.shutdown();
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+		for (int i = 0; i < threadAdapters.length; i++) {
+			threadAdapters[i].stop0();
+			waitThreadStop(threadAdapters[i]);
 		}
 	}
 
-	private class ServerHandler extends ChannelInboundHandlerAdapter {
-
-		@Override
-		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-			// 收到消息直接打印输出
-			System.out.println(ctx.channel().remoteAddress() + " Say : " + msg);
-
-			// 返回客户端消息 - 我已经接收到了你的消息
-			ctx.writeAndFlush("Received your message !\n");
+	/**
+	 * 等待线程停止
+	 * 
+	 * @param thread
+	 */
+	private void waitThreadStop(Thread thread) {
+		String threadName = thread.getName();
+		while (thread.isAlive()) {
+			try {
+				thread.join(1000);
+			} catch (InterruptedException e) {
+				log.error(e.getMessage(), e);
+			}
+			log.info("Waiting for " + threadName + " End.");
 		}
-
-		@Override
-		public void channelActive(ChannelHandlerContext ctx) throws Exception {
-			System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
-
-			ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
-
-			super.channelActive(ctx);
-		}
+		log.info(threadName + " End.");
 	}
+
 }
